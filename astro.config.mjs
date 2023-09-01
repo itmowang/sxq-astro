93328319c98d9411aaa4445723311b1463aa2d9a
itmowang <mw@loli.wang>
2023-09-02 04:51:50 +0800

    init: commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://blog2.loli.wang',
+  // 规范链接
+  outDir: "./docs" // 打包输出路径
+  ,
+  integrations: [tailwind()]
+});
---
+++ src/theme-simple/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    site:{
+        url:"/",
+        title:"Mowang"
+    }
+}
