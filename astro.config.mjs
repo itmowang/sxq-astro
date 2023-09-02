e99b0970883a7c6291cb5492c72b82cc2f45dd30
itmowang <mw@loli.wang>
2023-09-02 15:05:47 +0800

    fix: image 改为 img

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: 'https://blog2.loli.wang',
   // 规范链接
-  outDir: "./docs" // 打包输出路径
-  ,
+  outDir: "./docs", // 打包输出路径
   integrations: [tailwind()]
 });
