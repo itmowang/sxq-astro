9b690072660156f03be751bf1ad77a96b9125454
itmowang <mw@loli.wang>
2023-09-03 01:00:24 +0800

    fix：添了个油，加了个醋

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from 'astro/config'; 
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog2.loli.wang',
   // 规范链接
   outDir: "./docs", // 打包输出路径
+  integrations: [mdx(), sitemap()],
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
